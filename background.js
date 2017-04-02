ee6b31992d804776db0d222fe451ae5173b75d78
brigkann <brigkann@greeny.cs.tlu.ee>
2017-04-02 21:24:35 +0300

    started

---
+++ background.js
@@ -0,0 +1,36 @@
+console.log("loaded extension!");
+<html>
+<head>
+    <title></title>
+    <script src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+    google.load('search', '1');
+    google.setOnLoadCallback(OnLoad);
+    var search;
+
+
+
+    function OnLoad()
+    {
+        search = new google.search.ImageSearch();
+
+        search.setSearchCompleteCallback(this, searchComplete, null);
+
+        search.execute(keyword);
+    }
+
+    function searchComplete()
+    {
+        if (search.results && search.results.length > 0)
+        {
+            var rnd = Math.floor(Math.random() * search.results.length);
+
+            document.body.style.backgroundImage = "url('" + search.results[rnd]['url'] + "')";
+        }
+    }
+    </script>
+</head>
+<body>
+
+</body>
+</html>
