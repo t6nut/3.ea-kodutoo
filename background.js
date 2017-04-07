fbe44584697553f49039b10a429b55e070133e31
t6nut <t6nut@tlu.ee>
2017-04-07 08:43:42 +0300

    added keyword

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ console.log("loaded extension!");
     google.load('search', '1');
     google.setOnLoadCallback(OnLoad);
     var search;
-    var keyword = "meow";
+    var keyword = "cats";
 
     function OnLoad()
     {
