4564f8aded19c8dc58d402d029dc59eaa8022af4
Tõnu Trubetsky <t6nut@users.noreply.github.com>
2017-06-12 19:19:07 +0300

    Add files via upload

--- background.js
+++ background.js
@@ -1,25 +1,34 @@
-console.log("loaded extension!");
 
-    google.load('search', '1');
-    google.setOnLoadCallback(OnLoad);
-    var search;
-    var keyword = "cats";
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  var items = Array("normal","italic","oblique","initial","inherit");
+  var item = items[Math.floor(Math.random()*items.length)];
+  var query = document.querySelectorAll('p, div');
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.insertCSS({
+    code: 'p {color:pink;}div {color:red;}'
+  });
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")", document.body.style.fontStyle = "italic", document.body.style.fontFamily ="Impact,Charcoal,sans-serif", document.body.style.color = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")";'
+  });
+});
+colorLinks("#555586");
 
-    function OnLoad()
+function colorLinks(hex)
+{
+    var links = document.getElementsByTagName("a");
+    for(var i=0;i<links.length;i++)
     {
-        search = new google.search.ImageSearch();
-
-        search.setSearchCompleteCallback(this, searchComplete, null);
-
-        search.execute(keyword);
-    }
-
-    function searchComplete()
-    {
-        if (search.results && search.results.length > 0)
+        if(links[i].href)
         {
-            var rnd = Math.floor(Math.random() * search.results.length);
-
-            document.body.style.backgroundImage = "url('" + search.results[rnd]['url'] + "')";
+            links[i].style.color = hex;
         }
     }
+}
+/*document.getElementById('btn').onclick = function() {
+            var val = document.getElementById('imagename').value,
+                src = 'http://webpage.com/images/' + val +'.png',
+                img = document.createElement('img');
+
+            img.src = src;
+            document.body.appendChild(img);*/
